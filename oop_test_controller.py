3e4ce69b6e236816d4cf15d18f84d780af255469
Adele D'Souza-Monie <adele.d'souza-monie@student.onslow.school>
2022-11-11 10:02:08 +1300

    end of sprint 4 commit

--- oop_test_controller.py
+++ oop_test_controller.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass
-from PySide6.QtWidgets import (QVBoxLayout, QPushButton, QLabel, QRadioButton, QMessageBox, QMainWindow)
+from PySide6.QtWidgets import (QVBoxLayout, QPushButton, QLabel, QRadioButton, QMessageBox, QMainWindow, QWidget, QHBoxLayout)
 from PySide6.QtGui import(QPixmap)
 from oop_test_model import oop_questions, current_question_index
 import os
+from PySide6.QtCore import Qt
+
 @dataclass
 class OOPQuestionController:
     """Class for controlling the GUI"""
+    main_window: QMainWindow
     main_layout: QVBoxLayout
     dialouge_label: QLabel
+    bottom_button_widget: QPushButton
     next_button: QPushButton 
+    back_button: QPushButton
     points_label: QLabel
+    oop_quiz_label: QLabel
     code_snip_label: QLabel
     question_label: QLabel
     answer_one_button: QRadioButton
@@ -22,27 +28,55 @@ class OOPQuestionController:
     reward_info_label: QLabel
     certain_reward_button: QPushButton
     uncertain_reward_button: QPushButton
+    top_widget : QWidget
+    top_layout: QHBoxLayout
+    middle_widget: QWidget
+    middle_layout: QHBoxLayout
+    left_middle_widget: QWidget
+    left_middle_vbox: QVBoxLayout
+    right_middle_widget: QWidget
+    right_middle_vbox: QVBoxLayout
+    bottom_widget: QWidget
+    bottom_layout: QHBoxLayout
 
-    def next_button_clicked(self, checked: bool):
+    def get_current_dialouge(self):
+        """Gets the text of the current how to play dialouge that's showing"""
         # Explain game in slides
         # List of dialouges
         how_to_play = [
-            ("The OOP quiz contains a series of questions relating to oop"),
-            ("Answer correctly to get 100 points"),
-            ("After answering a question, choose to gamble or recieve the 100 dollar reward if you answer the next question correctly"),
+            ("The OOP quiz contains a\nseries of questions\nrelating to oop"),
+            ("Answer correctly to get 100\npoints"),
+            ("After answering a question,\nchoose to gamble or recieve\nthe 100 dollar reward if you\nanswer the next question\ncorrectly"),
             ("You start with 100 points"),
             ("Let's begin")
         ]
-        # Set text based on what is currently in the dialouge label
         # If its showing the welcome message
         if self.dialouge_label.text() == ("Welcome to the OOP Quiz!"):
-            self.dialouge_label.setText(how_to_play[0])
-        # If its showing the last message,bring up first question 
-        elif self.dialouge_label.text() == how_to_play[len(how_to_play) - 1]:
-            self.show_question()
+            index = -1
+            next_text = how_to_play[index + 1]
+            back_text = ""
+        # If its on the last dialouge
+        elif self.dialouge_label.text() == ("Let's begin"):
+            index = 4
+            next_text = ""
+            back_text = how_to_play[index - 1]
         else:
-            string_index = how_to_play.index(self.dialouge_label.text())
-            self.dialouge_label.setText(how_to_play[string_index + 1])
+            index = how_to_play.index(self.dialouge_label.text()) 
+            next_text = how_to_play[index + 1]
+            back_text = how_to_play[index - 1]
+        return next_text, back_text, index
+
+    def next_button_clicked(self, checked: bool):
+        # Set text based on what is currently in the dialouge label
+        next_text, back_text, index = self.get_current_dialouge()
+        self.dialouge_label.setText(next_text)
+        # If the text has changed from the welcome label, show the back button to the gui
+        if back_text == "":
+            self.main_layout.addWidget(self.back_button)
+            self.back_button.show()
+        # If its showing the last message, bring up question page
+        elif next_text == "":
+            self.show_question()
         # User starts with 100 points, on the first question, if they answer correctly they get another 100, incorrect, they get no points
         global points
         points = 100
@@ -50,7 +84,15 @@ class OOPQuestionController:
         points_receivable = 100
         global points_deductable
         points_deductable = 0
-        # for functionality/development, (back and forth with buttons)
+        
+    def back_button_clicked(self):
+        # Set text based on what is currently in the dialouge label
+        next_text, back_text, index = self.get_current_dialouge()
+        self.dialouge_label.setText(back_text)
+        # If we're going back from the first dialoge slide, remove back button and show welcome message
+        if index == 0:
+            self.back_button.hide()
+            self.dialouge_label.setText("Welcome to the OOP Quiz!")
     
     def get_current_question(self):
         # Get list of question objects
@@ -68,18 +110,53 @@ class OOPQuestionController:
         if current_question_index == 1:
             self.main_layout.removeWidget(self.dialouge_label)
             self.main_layout.removeWidget(self.next_button)
+            self.main_layout.removeWidget(self.back_button)
             self.dialouge_label.deleteLater()
             self.next_button.deleteLater()
-        # Set up question page
-        self.main_layout.addWidget(self.points_label)
-        self.main_layout.addWidget(self.code_snip_label)
-        self.main_layout.addWidget(self.question_label)
-        self.main_layout.addWidget(self.answer_one_button)
-        self.main_layout.addWidget(self.answer_two_button)
-        self.main_layout.addWidget(self.answer_three_button)
-        global answer_buttons
-        answer_buttons = [self.answer_one_button, self.answer_two_button, self.answer_three_button]
-        self.main_layout.addWidget(self.submit_button)
+            self.back_button.deleteLater()
+            self.main_layout.removeWidget(self.bottom_button_widget)
+            self.bottom_button_widget.deleteLater()
+            # Set up and style question page
+            # Top
+            self.main_layout.addWidget(self.top_widget)
+            self.top_widget.setLayout(self.top_layout)
+            self.top_layout.addWidget(self.oop_quiz_label)
+            self.oop_quiz_label.setAlignment(Qt.AlignTop)
+            self.top_layout.addWidget(self.points_label)
+            self.points_label.setAlignment(Qt.AlignTop)
+            self.points_label.setAlignment(Qt.AlignRight)
+            self.top_widget.setStyleSheet("QWidget { font-size : 16px }")
+            # Middle 
+            middle_widget = QWidget()
+            self.main_layout.addWidget(middle_widget)
+            middle_layout = QHBoxLayout()
+            middle_widget.setLayout(middle_layout)
+            left_middle_widget = QWidget()
+            middle_layout.addWidget(left_middle_widget)
+            left_middle_vbox = QVBoxLayout()
+            left_middle_widget.setLayout(left_middle_vbox)
+            left_middle_vbox.addWidget(self.question_label)
+            self.question_label.setAlignment(Qt.AlignCenter)
+            left_middle_widget.setStyleSheet("QWidget { font-size : 16px }")
+            left_middle_vbox.addWidget(self.code_snip_label)
+            right_middle_widget = QWidget()
+            middle_layout.addWidget(right_middle_widget)
+            right_middle_vbox = QVBoxLayout()
+            right_middle_widget.setLayout(right_middle_vbox)
+            right_middle_vbox.addWidget(self.answer_one_button)
+            right_middle_vbox.addWidget(self.answer_two_button)
+            right_middle_vbox.addWidget(self.answer_three_button)
+            global answer_buttons
+            answer_buttons = [self.answer_one_button, self.answer_two_button, self.answer_three_button]
+            # Bottom
+            bottom_widget = QWidget()
+            self.main_layout.addWidget(bottom_widget)
+            bottom_layout = QHBoxLayout()
+            bottom_widget.setLayout(bottom_layout)
+            bottom_layout.addWidget(self.submit_button)
+        # Set the answer status to null if its the first question
+        global answer_status
+        answer_status = ""
         # Get the current question
         current_question = self.get_current_question()
         # Add infomation about the question to GUI
@@ -95,15 +172,17 @@ class OOPQuestionController:
         answer_list = current_question.answer_list 
         # Display points
         global points
-        self.points_label.setText(str(points))
-        # Reset the answer status
-        global answer_status
-        answer_status = ""
+        self.points_label.setText(f"Points: {points}")
+        # Set all the answer buttons to unchecked
         # Add answer text to buttons
         for button in answer_buttons:
+            button.setAutoExclusive(False)
+            button.setChecked(False)
             index = answer_buttons.index(button)
             answer = answer_list[index]
             button.setText(answer)
+            # Make the buttons auto exclusive again
+            button.setAutoExclusive(True)
 
     def answer_button_clicked(self):
         """"Get the selected answer""" 
@@ -123,35 +202,40 @@ class OOPQuestionController:
             answer_status = False
         print(answer_status)
         
-        # for functionality/development, (check highlight button selected) (Change from push to radio buttons)
-
     def submit_button_clicked(self, checked: bool):
         # Check that a radio button has been selected
         if answer_status == "":
-            QMessageBox(QMessageBox.Icon.Warning, f"Error", f"Please select an answer before moving on")
+            QMessageBox(QMessageBox.Icon.Warning, f"Error", f"Please select an answer before moving on").exec()
+        # If the current question is the last, then show the last page
+        elif current_question_index == 12:
+            self.show_end_page()
         else:
-            # clear all the question page widgets
             # bring up a window saying if answer is correct or not, post informational dialouge
             self.secondary_window.show()
+            self.main_window.hide()
             global points
             global points_receivable
             global points_deductable
             if answer_status == True:
-                self.answer_status_label.setText("Correct")
+                self.answer_status_label.setText(f"Correct!\nYou have won {points_receivable} points!")
                 points += points_receivable
+                self.question_info_label.setText("")
             else:
-                self.answer_status_label.setText("Incorrect")
+                if points_deductable == 0:
+                    self.answer_status_label.setText(f"Incorrect.\nYour points stay the same.")
+                else:
+                    self.answer_status_label.setText(f"Incorrect.\nYou have lost {points_deductable} points!")
                 points -= points_deductable
-            # Display info about the current question
-            info = self.get_current_question().info
-            self.question_info_label.setText(info)
-
-        # for usability, show the correct answer if answer is wrong
+                # Display info about the current question
+                info = self.get_current_question().info
+                self.question_info_label.setText(info)
 
     def certain_reward_button_clicked(self):
         """If user chooses the certain reward, set their points receivable to 100"""
         global points_receivable
         points_receivable = 100
+        global points_deductable
+        points_deductable = 0
         
         # Increment current index # Reset the answer status for the next question
         global current_question_index
@@ -159,6 +243,7 @@ class OOPQuestionController:
         current_question_index = new_question_index
         # Ask next question
         self.secondary_window.hide()
+        self.main_window.show()
         self.show_question()
         
     def uncertain_reward_button_clicked(self):
@@ -174,19 +259,52 @@ class OOPQuestionController:
         current_question_index = new_question_index
         # Ask next question
         self.secondary_window.hide()
+        self.main_window.show()
         self.show_question()
+    
+    def show_end_page(self):
+        # Remove all widgets from the question page
+        self.top_layout.removeWidget(self.oop_quiz_label)
+        self.top_layout.removeWidget(self.points_label)
+        self.main_layout.removeWidget(self.top_widget)
+        self.oop_quiz_label.deleteLater()
+        points = self.points_label.text()
+        self.points_label.deleteLater()
+        self.top_widget.deleteLater()
+        self.main_layout.removeWidget(self.middle_widget)
+        self.middle_layout.removeWidget(self.left_middle_widget)
+        self.left_middle_vbox.removeWidget(self.question_label)
+        self.left_middle_vbox.removeWidget(self.code_snip_label)
+        self.middle_layout.removeWidget(self.right_middle_widget)
+        self.right_middle_vbox.removeWidget(self.answer_one_button)
+        self.right_middle_vbox.removeWidget(self.answer_two_button)
+        self.right_middle_vbox.removeWidget(self.answer_three_button)
+        self.main_layout.removeWidget(self.bottom_widget)
+        self.bottom_layout.removeWidget(self.submit_button)
+        self.middle_widget.deleteLater()
+        self.left_middle_widget.deleteLater()
+        self.question_label.deleteLater()
+        self.code_snip_label.deleteLater()
+        self.right_middle_widget.deleteLater()
+        self.answer_one_button.deleteLater()
+        self.answer_two_button.deleteLater()
+        self.answer_three_button.deleteLater()
+        self.bottom_widget.deleteLater()
+        self.submit_button.deleteLater()
+        # Set up the final page
+        final_points_label = QLabel(f"Game Over.\n\nTotal {points}\nCongratulations")
+        self.main_layout.addWidget(final_points_label)
+        final_points_label.setAlignment(Qt.AlignCenter)
+        final_points_label.setStyleSheet("QLabel { font-size: 16px, }")
+
 
     def __post_init__(self):
         """Connect signal functions to GUI"""
         self.next_button.clicked.connect(self.next_button_clicked)
+        self.back_button.clicked.connect(self.back_button_clicked)
         self.answer_one_button.clicked.connect(self.answer_button_clicked)
         self.answer_two_button.clicked.connect(self.answer_button_clicked)
         self.answer_three_button.clicked.connect(self.answer_button_clicked)
         self.submit_button.clicked.connect(self.submit_button_clicked)
         self.certain_reward_button.clicked.connect(self.certain_reward_button_clicked)
         self.uncertain_reward_button.clicked.connect(self.uncertain_reward_button_clicked)
-
-# Loop, update points
-
-# Once all questions are asked, show end messages
-# Show leaderboard
--- oop_test_view.py
+++ oop_test_view.py
@@ -1,17 +1,15 @@
 from os import fsdecode
-from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QRadioButton)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton)
 from oop_test_model import oop_questions
 from oop_test_controller import OOPQuestionController
+from PySide6.QtCore import Qt
 
 # Set up app and main window
 app = QApplication()
 main_window = QMainWindow()
 main_window.setWindowTitle("OOP Quiz")
 main_window.resize(700, 500)
-
-secondary_window = QMainWindow()
-secondary_window.setWindowTitle("Hilo")
-
+app.setStyleSheet("QWidget { color : rgb(30, 30, 30); font-size : 14px; font-family : courier new; }")
 
 # Set up main widget
 main_widget = QWidget()
@@ -19,43 +17,108 @@ main_window.setCentralWidget(main_widget)
 main_layout = QVBoxLayout()
 main_widget.setLayout(main_layout)
 
-# Set up second window widget
+reward_info_label = QLabel("Select your reward for the next question below: ")
+certain_reward_button = QPushButton("Correct answer gets 100pts, a Wrong answer gets 0 pts")
+uncertain_reward_button = QPushButton("Correct answer gets x2 points, Wrong answer halves my points")
+
+# Set up second window 
+secondary_window = QMainWindow()
+secondary_window.setWindowTitle("Bet Page")
+secondary_window.resize(700, 500)
 secondary_widget = QWidget()
 secondary_window.setCentralWidget(secondary_widget)
 secondary_layout = QVBoxLayout()
 secondary_widget.setLayout(secondary_layout) 
-
 answer_status_label = QLabel() # Displays if selected answer is right or wrong
 question_info_label = QLabel() # Displays information about the question
 secondary_layout.addWidget(answer_status_label)
+answer_status_label.setStyleSheet("QLabel { font-size: 16px; }")
+answer_status_label.setAlignment(Qt.AlignCenter)
 secondary_layout.addWidget(question_info_label)
-
-reward_info_label = QLabel("Select your reward for the next question")
-certain_reward_button = QPushButton("Get 100 points if i answer correctly and 0 points if answer incorrectly")
-uncertain_reward_button = QPushButton("Double my points if i answer correctly and half my points if i answer incorrectly")
-
+question_info_label.setAlignment(Qt.AlignCenter)
 secondary_layout.addWidget(reward_info_label)
+reward_info_label.setStyleSheet("QLabel { font-size: 16px; }")
+reward_info_label.setAlignment(Qt.AlignCenter)
 secondary_layout.addWidget(certain_reward_button)
 secondary_layout.addWidget(uncertain_reward_button)
-
+secondary_window.setStyleSheet("""
+    QPushButton { 
+        background-color : rgb(251, 203, 7); 
+        border-radius: 10%; 
+        height: 50px; 
+        width: 50%; 
+        font-size : 15px
+    }
+    QPushButton:hover {
+        color: white;
+        border: 2px
+    }
+""")
+certain_reward_button.setStyleSheet("QPushButton { background-color: rgb(135, 205, 248) }")
 # Set up initial page
 dialouge_label = QLabel("Welcome to the OOP Quiz!")
+dialouge_label.setAlignment(Qt.AlignCenter)
+dialouge_label.setStyleSheet("QLabel { margin-top: 175px, }")
 next_button = QPushButton("Next")
+back_button = QPushButton("Back")
+back_button.setStyleSheet("""
+    QPushButton { 
+        background-color : rgb(135, 205, 248); 
+        border-radius: 10%; 
+        height: 50px; 
+        width: 50%; 
+        font-size : 15px
+    }
+    QPushButton:hover {
+        color: rgb(135, 205, 248);
+        border: 2px
+    }
+""")
+main_window.setStyleSheet("""
+    QPushButton { 
+        background-color : rgb(251, 203, 7); 
+        border-radius: 10%; 
+        height: 50px; 
+        width: 50%; 
+        font-size : 15px
+    }
+    QPushButton:hover {
+        color: white;
+        border: 2px
+    }
+""")
 main_layout.addWidget(dialouge_label)
+# bottom
+bottom_button_widget = QWidget()
+main_layout.addWidget(bottom_button_widget)
+bottom_hbox = QHBoxLayout()
+bottom_button_widget.setLayout(bottom_hbox)
 main_layout.addWidget(next_button)
-
 # Set up question page
 points_label = QLabel()
+oop_quiz_label = QLabel("OOP Quiz")
 code_snip_label = QLabel()
 question_label = QLabel()
 answer_one_button = QRadioButton()
 answer_two_button = QRadioButton()
 answer_three_button = QRadioButton()
 submit_button = QPushButton("Submit")
-
+top_widget = QWidget()
+top_layout = QHBoxLayout()
+middle_widget = QWidget()
+middle_layout = QHBoxLayout()
+left_middle_widget = QWidget()
+left_middle_vbox = QVBoxLayout()
+right_middle_widget = QWidget()
+right_middle_vbox = QVBoxLayout()
+bottom_widget = QWidget()
+bottom_layout = QHBoxLayout()
 
 # Set up controller
-controller = OOPQuestionController(main_layout, dialouge_label, next_button, points_label, code_snip_label, question_label, answer_one_button, answer_two_button, answer_three_button, submit_button, secondary_window, answer_status_label, question_info_label, reward_info_label, certain_reward_button, uncertain_reward_button)
+controller = OOPQuestionController(main_window, main_layout, dialouge_label, bottom_button_widget, next_button, back_button, points_label, oop_quiz_label, code_snip_label, question_label, answer_one_button, answer_two_button, answer_three_button, submit_button, secondary_window, answer_status_label, question_info_label, reward_info_label, certain_reward_button, uncertain_reward_button, top_widget, top_layout, middle_widget, middle_layout, left_middle_widget, left_middle_vbox,right_middle_widget,
+    right_middle_vbox,
+    bottom_widget,
+    bottom_layout,)
 
 main_window.show()
 app.exec()
